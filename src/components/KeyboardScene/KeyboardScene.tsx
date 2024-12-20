5e69eb7a9cc43c70f3a2347378e8fd2759e044f6
Frederic MAMATH <fredericmamath@gmail.com>
2024-12-20 23:38:41 +0100

    :construction: Changes camera to isometric view

--- src/components/KeyboardScene/KeyboardScene.tsx
+++ src/components/KeyboardScene/KeyboardScene.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 import KeyboardModel from "../KeyboardModel/KeyboardModel";
 
 interface Props {
@@ -9,10 +9,16 @@ interface Props {
 const KeyboardScene = ({ keyPressed }: Props) => {
   return (
     <Canvas>
+      <OrthographicCamera
+        makeDefault
+        zoom={100}
+        position={[10, 10, 10]}
+        rotation={[-Math.PI / 4, Math.PI / 4, 0]}
+      />
       <ambientLight intensity={0.8} />
       <directionalLight position={[10, 10, 5]} intensity={1.5} />
       <KeyboardModel keyPressed={keyPressed} />
-      <OrbitControls />
+      <OrbitControls enabled={false} />
     </Canvas>
   );
 };
